6ae3f0a716eace741878434c052bf005c8db2ef6
Kevin <kevinpatel0624@gmail.com>
2021-03-31 17:39:17 -0400

    -Tried some stuff out with a bitcoin ticker api

--- Network_Practice.py
+++ Network_Practice.py
@@ -1,5 +1,12 @@
 import socket
+import requests, json, time
 
-mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# AF.INET creates the socket and sock_stream has characters come one after another
-mySocket.connect(('data.pr4e.org', 80))
-print(mySocket.getsockname())
+# mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# AF.INET creates the socket and sock_stream has characters come one after another
+# mySocket.connect(('data.pr4e.org', 80))
+# print(mySocket.getsockname())
+
+api = "https://blockchain.info/ticker"
+apinew = "https://blockchain.info/tobtc?currency=USD&value=500"
+response = requests.get(api)
+set = []
+print(response.json())
